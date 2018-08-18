e8b375a4c754134de3db5d33fd86b132fdb3f4fc
avral <qweasd_96@mail.ru>
2018-08-19 02:59:18 +0300

    fix map css

--- pages/index.vue
+++ pages/index.vue
@@ -30,23 +30,3 @@ export default {
 }
 
 </script>
-
-<style scoped>
-#map {
-  top: 78px;
-  position: sticky;
-  height: calc(100vh - 92px);
-
-  border-radius: 6px;
-  -webkit-box-shadow: 0 0 10px 0 rgba(0,0,0,.1);
-  box-shadow: 0 0 10px 0 rgba(0,0,0,.1);
-  z-index: 10;
-  overflow: hidden;
-}
-.right-fixed-container {
-  padding-left: 0px;
-  top: 85px;
-  position: sticky;
-  height: calc(100vh - 116px);
-}
-</style>
